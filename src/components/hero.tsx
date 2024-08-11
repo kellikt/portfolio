086afeb4e1bdd633c1d36173aba6142f876d9820
kellikt <kellikt@hawaii.edu>
2024-08-11 08:59:48 -1000

    setup hero

---
+++ src/components/hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <div className="py-32">
+      <h1 className="font-heading font-bold text-8xl uppercase mb-5">
+        Kelli Tamashiro
+      </h1>
+      <p className="text-center text-4xl uppercase">Full-Stack Developer</p>
+    </div>
+  );
+}
